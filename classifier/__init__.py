f27613492d0e3d6acaa9bc357e18b884175debf9
Rob Harries <rob@igecko.com>
2022-09-24 21:59:43 +0000

    Few Shot classifier with gaussian class variance

--- classifier/__init__.py
+++ classifier/__init__.py
@@ -2,3 +2,4 @@ from .base import FewShotClassifier
 from .weighted_average import WeightedTextFewShotClassifier
 from .hard_prompt import HardPromptFewShotClassifier
 from .nearest_neighbors import NearestNeighborFewShotClassifier
+from .gaussian import GaussianFewShotClassifier
